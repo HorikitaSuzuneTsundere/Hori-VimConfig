f9f50274417b48e26e778b93e589fc7ba5b462e7
HORI <e.masaga.476136@umindanao.edu.ph>
2025-01-29 08:49:14 +0800

    Disable automated installation of lua servers

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -16,4 +16,25 @@ return {
       },
     },
   },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      local exclude_pkgs = {
+        ["lua-language-server"] = true,
+        ["shfmt"] = true,
+        ["stylua"] = true,
+      }
+
+      -- Ensure `opts.ensure_installed` is valid before applying the filter
+      if opts.ensure_installed and type(opts.ensure_installed) == "table" then
+        local filtered = {}
+        for _, pkg in ipairs(opts.ensure_installed) do
+          if not exclude_pkgs[pkg] then
+            table.insert(filtered, pkg)
+          end
+        end
+        opts.ensure_installed = filtered
+      end
+    end,
+  },
 }
