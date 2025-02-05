1fb86911f4560cbbcdb6b7e05ec2c7907ade13f2
HORI <e.masaga.476136@umindanao.edu.ph>
2025-02-05 08:50:15 +0800

    Optimized snacks_picker

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -14,7 +14,42 @@ return {
       scroll = {
         enabled = false, -- Disable smooth scrolling
       },
+      picker = {
+        -- Core performance settings
+        performance = {
+          async_loading = true, -- Non-blocking loading
+          cache = {
+            enabled = true,     -- Enable caching
+            max_entries = 100,  -- Optimal for most users
+            ttl = 3600,         -- 1 hour cache expiration
+            eviction_policy = "LRU",
+          },
+          debounce = 100, -- Faster than default 150ms
+          throttle = 50   -- Render interval in ms
+        },
+        -- Render optimizations
+        layout = {
+          width = 0.85,        -- Optimal screen coverage
+          height = 0.75,
+          preview_cutoff = 50, -- Disable preview for large results
+          horizontal = {       -- Better performance than vertical
+            mirror = false,
+            preview_width = 0.45
+          },
+          vertical = {
+            mirror = false,
+            preview_height = 0.35,
+          },
+        },
+        features = {
+          animations = false,       -- Biggest performance gain
+          image_previews = false,
+          nutritional_info = false, -- Changed from calorie_counter
+          social_sharing = false
+        },
+      },
     },
+    event = "VeryLazy"
   },
   {
     "williamboman/mason.nvim",
