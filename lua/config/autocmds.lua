3e60bcf6ac1f9fc841703715d71dc2acf613b425
HORI <e.masaga.476136@umindanao.edu.ph>
2025-03-11 09:20:18 +0800

    created date modifier

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -13,3 +13,26 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     vim.opt.fillchars = { eob = " " } -- Removes ~ from empty lines
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        local buf = 0
+        local current_date = os.date("%Y-%m-%d")
+
+        -- Read line 5 efficiently
+        local line = vim.api.nvim_buf_get_lines(buf, 4, 5, false)[1]
+        if not line then return end
+
+        -- Locate `[YYYY-MM-DD]`
+        local start_idx, end_idx = line:find("%[%d%d%d%d%-%d%d%-%d%d%]")
+        if not start_idx then return end
+
+        -- Replace date while preserving brackets
+        local new_line = line:sub(1, start_idx) .. current_date .. line:sub(end_idx)
+        if new_line == line then return end
+
+        -- Update only if necessary
+        vim.api.nvim_buf_set_lines(buf, 4, 5, false, { new_line })
+    end,
+})
